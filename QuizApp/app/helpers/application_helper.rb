19835e2280b75a6dafda8d10f68caefc575d40a1
kimani <kimaniowens@gmail.com>
2014-07-31 10:09:32 -0500

    customize routes, update 'cities' views, generate helper methods

--- QuizApp/app/helpers/application_helper.rb
+++ QuizApp/app/helpers/application_helper.rb
@@ -1,2 +1,35 @@
 module ApplicationHelper
+	# Here, we'll have a master list of answer and the user's
+	#guess will be checked agianst that list.
+
+	def check_answer(answer)
+		citylist = ["atlanta", "Asheville", "Charlotte", "Raleigh"]
+	# Additional master-lists could be placed here.
+
+	correct = false		
+
+	citylist.each do |c|
+
+		if answer == c
+
+			correct = true
+		end
+	end
+	return correct
 end
+
+	# Check to see if the answer, while correct, may have already
+	# Been guessed and put into the database of answers.
+def check_for_dup(answer, database)
+	is_dup = false
+
+	databse.each do |d|
+	if answer == d.name
+
+	is_dup = true
+		end
+	end
+	return is_dup
+	end	
+end
+
--- QuizApp/config/routes.rb
+++ QuizApp/config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
+  resources :cities
+
+  get 'cities/youwon' => 'cities#youwon' :as :youwon #helper tag insertion
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
